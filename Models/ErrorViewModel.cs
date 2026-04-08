c0cfe1905a165ff5aaf74524e8140a360a2b7467
raniaalafinsh <alafinshrania2@gmail.com>
2026-04-08 22:06:53 +0300

    cleanup: removed duplicated root project structure

--- Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace SamiSpot.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
--- ViewModels/LoginViewModel.cs.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace SamiSpot.ViewModels
-{
-    public class LoginViewModel
-    {
-        public string Email { get; set; }
-        public string Password { get; set; }
-    }
-}
