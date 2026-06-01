966e08b5eabb88c891edcaeb78ff4e6c6dc9007f
RayanMT <157949161+RayanMT@users.noreply.github.com>
2026-06-01 11:59:50 +0300

    US34: allow AI to use current location (BSPMT15-212)

--- SamiSpot/Controllers/AiChatController.cs
+++ SamiSpot/Controllers/AiChatController.cs
@@ -15,8 +15,9 @@ namespace SamiSpot.Controllers
         {
             _context = context;
             _ai = ai;
-        }
 
+        }
+        // US34: allow AI to use current location (BSPMT15-212)
         private int? GetCurrentUserId()
         {
             var userIdStr = HttpContext.Session.GetString("UserId");
--- SamiSpot/obj/Debug/net8.0/SamiSpot.AssemblyInfo.cs
+++ SamiSpot/obj/Debug/net8.0/SamiSpot.AssemblyInfo.cs
@@ -16,7 +16,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SamiSpot")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+78efe4388b7fa1d464d505cbe8ed330bcb231bb4")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2d95e9843ad27d295c908230a62daeca1f021c92")]
 [assembly: System.Reflection.AssemblyProductAttribute("SamiSpot")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SamiSpot")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
